3df769b0aa817fddf4d34303d77ce9e123b6ac1f
Neerajkbishnoi <neerajk.bishnoi29@gmail.com>
2024-10-31 12:26:08 +0530

    Initial commit

---
+++ src/Lab001.java
@@ -0,0 +1,6 @@
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello udita sharma");
+        System.out.println("hello udita sharma");
+    }
+}
---
+++ src/Lab002.java
@@ -0,0 +1,9 @@
+public class Lab002 {
+    public static void main(String[] args){
+        System.out.println(10+1);
+        System.out.println(10-1);
+        System.out.println((10*2));
+        System.out.println(20%2);
+        System.out.println(2^2);
+    }
+}
